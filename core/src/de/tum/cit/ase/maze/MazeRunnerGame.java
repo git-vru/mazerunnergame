da02a04ba05ea6a55d291fe48228aa8b5f81306d
vrushabhjain <vrushabh.jain@tum.de>
2024-01-11 19:12:00 +0100

    all assets load nicely

--- core/src/de/tum/cit/ase/maze/MazeRunnerGame.java
+++ core/src/de/tum/cit/ase/maze/MazeRunnerGame.java
@@ -165,26 +165,6 @@ public class MazeRunnerGame extends Game{
         characterUpAnimation = new Animation<>(0.1f, walkUpFrames);
         characterDownAnimation = new Animation<>(0.1f, walkDownFrames);
     }
-    /*private void loadMazeData(String fileName, List<int[]> mazeData) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] parts = line.split("=");
-                if (parts.length == 2) {
-                    String[] coordinates = parts[0].split(",");
-                    if (coordinates.length == 2) {
-                        int x = Integer.parseInt(coordinates[0]);
-                        int y = Integer.parseInt(coordinates[1]);
-                        int objectType = Integer.parseInt(parts[1]);
-                        mazeData.add(new int[] {x, y, objectType});
-                    }
-                }
-            }
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-    }*/
     private void loadMazeData(String fileName, Map<Point, Integer> mazeData) {
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String line;
@@ -223,14 +203,6 @@ public class MazeRunnerGame extends Game{
         }
     }
 
-
-    //    private void loadAllMazes() {
-//        List<int[]> mazeData = new ArrayList<>();
-//        for (int i = 1 ; i <= 5 ; i++) {
-//            loadMazeData("/Users/vrushabhjain/IdeaProjects/fophn2324infun2324projectworkx-g38/maps/level-"+i+".properties", mazeData);
-//            allMazes.add(mazeData);
-//        }
-//    }
     public void createMaze() {
         loadAllMazes();
         Map<Point, Integer> currentMazeData = allMazes.get(currentMazeIndex);
@@ -239,132 +211,42 @@ public class MazeRunnerGame extends Game{
         Texture wallTexture = new Texture("basictiles.png");
         Texture entryPointTexture = new Texture("things.png");
         Texture exitTexture = new Texture("things.png");
-        Texture trapTexture = new Texture("things.png");
+        Texture trapTexture = new Texture("basictiles.png");
         Texture enemyTexture = new Texture("mobs.png");
         Texture keyTexture = new Texture("objects.png");
-        TextureRegion wall1 = new TextureRegion(wallTexture, 0,0 /*wallTexture.getHeight() - 16*/, 16, 16);
-        TextureRegion entryPoint1 = new TextureRegion(entryPointTexture, 0, 0, 16, 16);
-        TextureRegion exit1 = new TextureRegion(exitTexture, 0, exitTexture.getHeight() - 32, 16, 16);
-        TextureRegion trap1 = new TextureRegion(trapTexture, 0, 0, 16, 16);
-        TextureRegion enemy1 = new TextureRegion(enemyTexture, 0, (enemyTexture.getHeight() / 2) - 16, 16, 16);
-        TextureRegion key1 = new TextureRegion(keyTexture, 0, keyTexture.getHeight() - 8, 8, 8);
+        TextureRegion wall = new TextureRegion(wallTexture, 0,0 , 16, 16);
+        TextureRegion entryPoint = new TextureRegion(entryPointTexture, 0, 0, 16, 16);
+        TextureRegion exit = new TextureRegion(exitTexture, 48, 0, 16, 16);
+        TextureRegion trap = new TextureRegion(trapTexture, 16, 32, 16, 16);
+        TextureRegion enemy = new TextureRegion(enemyTexture, 0,64, 16, 16);
+        TextureRegion key = new TextureRegion(keyTexture, 0, 64, 16, 16);
 
-        //List<int[]> currentMazeData = allMazes.get(currentMazeIndex);
         for (Map.Entry<Point, Integer> entry : currentMazeData.entrySet()) {
             Point point = entry.getKey();
             int x = point.x * 60;
             int y = point.y * 60;
             int objectType = entry.getValue();
             spriteBatch.begin();
-            /*if (currentMazeIndex == 0) {
-                switch (objectType) {
-                    case 0:
-                        spriteBatch.draw(entryPoint1, x, y, 60, 60);
-                        break;
-                    case 1:
-                        spriteBatch.draw(entryPoint1, x, y, 60, 60);
-                        break;
-                    case 2:
-                        spriteBatch.draw(exit1, x, y, 60, 60);
-                        break;
-                    case 3:
-                        spriteBatch.draw(trap1, x, y, 60, 60);
-                        break;
-                    case 4:
-                        spriteBatch.draw(enemy1, x, y, 60, 60);
-                        break;
-                    case 5:
-                        spriteBatch.draw(key1, x, y, 60, 60);
-                        break;
-                }
+            switch (objectType) {
+                case 0:
+                    spriteBatch.draw(wall, x, y, 60, 60);
+                    break;
+                case 1:
+                    spriteBatch.draw(entryPoint, x, y, 60, 60);
+                    break;
+                case 2:
+                    spriteBatch.draw(exit, x, y, 60, 60);
+                    break;
+                case 3:
+                    spriteBatch.draw(trap, x, y, 60, 60);
+                    break;
+                case 4:
+                    spriteBatch.draw(enemy, x, y, 60, 60);
+                    break;
+                case 5:
+                    spriteBatch.draw(key, x+15, y+15, 30, 30);
+                    break;
             }
-            else if (currentMazeIndex == 1) {
-                switch (objectType) {
-                    case 0:
-                        spriteBatch.draw(entryPoint1, x, y, 60, 60);
-                        break;
-                    case 1:
-                        spriteBatch.draw(entryPoint1, x, y, 60, 60);
-                        break;
-                    case 2:
-                        spriteBatch.draw(exit1, x, y, 60, 60);
-                        break;
-                    case 3:
-                        spriteBatch.draw(trap1, x, y, 60, 60);
-                        break;
-                    case 4:
-                        spriteBatch.draw(enemy1, x, y, 60, 60);
-                        break;
-                    case 5:
-                        spriteBatch.draw(key1, x, y, 60, 60);
-                        break;
-                }
-            }
-            else if (currentMazeIndex == 2) {
-                switch (objectType) {
-                    case 0:
-                        spriteBatch.draw(entryPoint1, x, y, 60, 60);
-                        break;
-                    case 1:
-                        spriteBatch.draw(entryPoint1, x, y, 60, 60);
-                        break;
-                    case 2:
-                        spriteBatch.draw(exit1, x, y, 60, 60);
-                        break;
-                    case 3:
-                        spriteBatch.draw(trap1, x, y, 60, 60);
-                        break;
-                    case 4:
-                        spriteBatch.draw(enemy1, x, y, 60, 60);
-                        break;
-                    case 5:
-                        spriteBatch.draw(key1, x, y, 60, 60);
-                        break;
-                }
-            }
-            else if (currentMazeIndex == 3) {
-                switch (objectType) {
-                    case 0:
-                        spriteBatch.draw(entryPoint1, x, y, 60, 60);
-                        break;
-                    case 1:
-                        spriteBatch.draw(entryPoint1, x, y, 60, 60);
-                        break;
-                    case 2:
-                        spriteBatch.draw(exit1, x, y, 60, 60);
-                        break;
-                    case 3:
-                        spriteBatch.draw(trap1, x, y, 60, 60);
-                        break;
-                    case 4:
-                        spriteBatch.draw(enemy1, x, y, 60, 60);
-                        break;
-                    case 5:
-                        spriteBatch.draw(key1, x, y, 60, 60);
-                        break;
-                }
-            }
-            else if (currentMazeIndex == 4) {*/
-                switch (objectType) {
-                    case 0:
-                        spriteBatch.draw(entryPoint1, x, y, 60, 60);
-                        break;
-                    case 1:
-                        spriteBatch.draw(entryPoint1, x, y, 60, 60);
-                        break;
-                    case 2:
-                        spriteBatch.draw(exit1, x, y, 60, 60);
-                        break;
-                    case 3:
-                        spriteBatch.draw(trap1, x, y, 60, 60);
-                        break;
-                    case 4:
-                        spriteBatch.draw(enemy1, x, y, 60, 60);
-                        break;
-                    case 5:
-                        spriteBatch.draw(key1, x, y, 60, 60);
-                        break;
-                }
             spriteBatch.end();
         }
     }
