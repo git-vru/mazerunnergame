68fb877bd42eaf81b376f9eff78bafe6d223e0ab
emirhan <go98pos@mytum.de>
2024-01-21 19:45:28 +0100

    trying to implement the option screen and enemy class

---
+++ core/src/de/tum/cit/ase/maze/FinishScreen.java
@@ -0,0 +1,90 @@
+package de.tum.cit.ase.maze;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+public class FinishScreen implements Screen {
+    private final Stage stage;
+    private final Texture backgroundTexture;
+    private final SpriteBatch batch;
+    private final MazeRunnerGame game;
+    private Hero hero;
+    public FinishScreen(MazeRunnerGame game) {
+        this.game = game;
+        var camera = new OrthographicCamera();
+        backgroundTexture = new Texture(Gdx.files.internal("foto.jpg"));
+        backgroundTexture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        batch = new SpriteBatch();
+        Viewport viewport = new ScreenViewport(camera);
+        stage = new Stage(viewport, game.getSpriteBatch());
+        Table table = new Table();
+        table.setFillParent(true);
+        stage.addActor(table);
+        table.add(new Label("YOU WON", game.getSkin(), "title")).padBottom(300).row();
+
+        TextButton goToMenu = new TextButton("Go To Menu", game.getSkin());
+        table.add(goToMenu).width(300).padBottom(15).row();
+        goToMenu.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                game.setScreen(new MenuScreen(game));
+            }
+        });
+    }
+
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+        batch.setProjectionMatrix(stage.getCamera().combined);
+        //hero.loadDanceAnimation();
+        batch.begin();
+        batch.draw(backgroundTexture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        //hero.winDraw(game.getSpriteBatch());
+        batch.end();
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+        stage.getCamera().position.set(stage.getCamera().viewportWidth / 2f, stage.getCamera().viewportHeight / 2f, 0);
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+    }
+
+    @Override
+    public void show() {
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void hide() {
+        //ADDED THIS BECAUSE THE LEVEL BUTTONS WERE STILL WORKING
+        //Gdx.input.setInputProcessor(null);
+    }
+}
